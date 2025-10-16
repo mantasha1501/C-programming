9e399a1307ce490bd16c2f1e15a9eb0138742ce8
mantasha1501 <mantasha.shaikh1501@gmail.com>
2025-10-16 19:16:00 +0530

    Add files via upload

---
+++ experiment1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    name : Mantasha Shaikh
+    age : 17
+    class : comps 01
+    division : B
+    UIN : 251P065
+    */
+
+    printf("Name : Mantasha Shaikh\n");
+    printf("Age : 17\n");
+    printf("Class : comps 01\n");
+    printf("Division : B\n");
+    printf("UIN : 251P065\n");
+
+    return 0;
+}
