2bc39674b5863b418a9391feb6a8ce0569119335
mantasha1501 <mantasha.shaikh1501@gmail.com>
2025-11-30 15:22:38 +0530

    Add files via upload

---
+++ experiment7a.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*mantasha shaikh
+uin: 251P065
+roll no: 55
+comps*/
+
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
